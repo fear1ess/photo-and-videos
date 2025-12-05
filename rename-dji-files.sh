16de725de12f354fee4cc0b782611e2cc7a7b92c
Sergii Pogoretskyi <sergii.pogoretskyi@globallogic.com>
2025-12-05 12:28:51 -0600

    feat: Rework variables. Logging. Dry run.

--- rename-dji-files.sh
+++ rename-dji-files.sh
@@ -1,33 +1,57 @@
 #!/bin/bash
-DRY_RUN=true
-SD_CARD_NAME=DJI
-cd /Volumes/$SD_CARD_NAME/DCIM/DJI_001
 
-echo -e "\n Clean WAV and LRF files \n"
-rm  *_D.WAV
-rm  *_D.LRF
+TARGET_NAME="Singles. LT. Against Chris"  # <-- set target name here
+DATE="20251202"                         # <-- set your target date here (YYYYMMDD)
+
+DRY_RUN=false
+# DRY_RUN=true
+DELETE_LRF=true
+DELETE_WAV=true
+
+SD_CARD_NAME=DJI
+WORKING_DIR=/Volumes/$SD_CARD_NAME/DCIM/DJI_001
+LOG_FILE="rename_log.txt"
+
+cd $WORKING_DIR || exit
+
+echo -e "\n Clean WAV=$DELETE_WAV and LRF=$DELETE_LRF files \n"
+if [[ "$DELETE_WAV" == true ]] && [[ "$DRY_RUN" != true ]] ; then rm ./*_D.WAV 2>/dev/null ; fi
+if [[ "$DELETE_LRF" == true ]] && [[ "$DRY_RUN" != true ]] ; then rm ./*_D.LRF 2>/dev/null ; fi
 
 echo -e "\n Directory content: \n"
 ls -ltr
 
 echo -e "\n Renaming: \n"
 
-TARGET_NAME="PPA_Lakeland. Men's 5.0+"    # <-- set target name here
-DATE="20251122"                 # <-- set your target date here (YYYYMMDD)
+
+# Start log section
+echo "---------------------------------------------" >> "$LOG_FILE"
+echo "Rename session: $(date)" >> "$LOG_FILE"
+echo "Target date: $DATE" >> "$LOG_FILE"
+echo "Dry run: $DRY_RUN" >> "$LOG_FILE"
+echo "---------------------------------------------" >> "$LOG_FILE"
 
 for f in DJI_*.MP4; do
   # extract date from filename
   file_date="${f:4:8}"
 
-  # skip if this file is not from DATE
+  # skip non-matching dates
   [[ "$file_date" != "$DATE" ]] && continue
 
+  # Build new filename
   new=$(printf '%s' "$f" | sed -E "s/^DJI_([0-9]{4})[0-9]{10}_0*([0-9]+)_D\.MP4$/\1 ${TARGET_NAME} \2.MP4/")
 
-  echo "$f --> $new"
-  # mv -v -- "$f" "$new"
+  if [[ "$DRY_RUN" == true ]]; then
+      echo "[DRY RUN] $f --> $new"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') [DRY RUN] $f --> $new" >> "$LOG_FILE"
+  else
+      echo "Renaming: $f --> $new"
+      mv -v -- "$f" "$new"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') Renamed: $f --> $new" >> "$LOG_FILE"
+  fi
 done
 
+echo -e "\nLog saved to: $LOG_FILE\n"
 
 
 # i=1
